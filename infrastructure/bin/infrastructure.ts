b9ee6f29058677d77d020f99a436ea7e52222787
Clare Liguori <liguori@amazon.com>
2018-10-28 18:56:40 -0700

    Remove unused

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import ec2 = require('@aws-cdk/aws-ec2');
-import ecs = require('@aws-cdk/aws-ecs');
+//import ecs = require('@aws-cdk/aws-ecs');
 import cdk = require('@aws-cdk/cdk');
 import { StaticSite } from './static-site';
 
@@ -8,7 +8,7 @@ class TriviaGameInfrastructureStack extends cdk.Stack {
     constructor(parent: cdk.App, name: string, domainName: string, siteSubDomain: string, props?: cdk.StackProps) {
         super(parent, name, props);
 
-        const vpc = new ec2.VpcNetwork(this, 'TriviaGameVPC', { maxAZs: 2 });
+        new ec2.VpcNetwork(this, 'TriviaGameVPC', { maxAZs: 2 });
 
         // Commenting out until this construct is merged into cdk release
         //const cluster = new ecs.FargateCluster(this, 'TriviaGameCluster', { vpc });
