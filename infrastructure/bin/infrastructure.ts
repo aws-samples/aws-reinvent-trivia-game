d7c9ecff522fc2da7e36d0f2b8c1540f130713c0
Clare Liguori <liguori@amazon.com>
2018-11-07 10:32:39 -0800

    Model ECS cluster

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import ec2 = require('@aws-cdk/aws-ec2');
-//import ecs = require('@aws-cdk/aws-ecs');
+import ecs = require('@aws-cdk/aws-ecs');
 import cdk = require('@aws-cdk/cdk');
 import { StaticSite } from './static-site';
 
@@ -8,11 +8,10 @@ class TriviaGameInfrastructureStack extends cdk.Stack {
     constructor(parent: cdk.App, name: string, domainName: string, siteSubDomain: string, props?: cdk.StackProps) {
         super(parent, name, props);
 
-        new ec2.VpcNetwork(this, 'TriviaGameVPC', { maxAZs: 2 });
+        const vpc = new ec2.VpcNetwork(this, 'TriviaGameVPC', { maxAZs: 2 });
 
-        // Commenting out until this construct is merged into cdk release
-        //const cluster = new ecs.FargateCluster(this, 'TriviaGameCluster', { vpc });
-        //cluster.export();
+        const cluster = new ecs.Cluster(this, 'TriviaGameCluster', { vpc });
+        cluster.export();
 
         new StaticSite(this, 'StaticSite', {
             domainName,
