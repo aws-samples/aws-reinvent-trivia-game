84291347fadb9bc479353815ea22242551047267
Clare Liguori <liguori@amazon.com>
2018-11-10 11:02:13 -0800

    Add TLS and custom domain to service

--- trivia-backend/cdk/ecs-service.ts
+++ trivia-backend/cdk/ecs-service.ts
@@ -1,25 +1,42 @@
 #!/usr/bin/env node
-import ec2 = require('@aws-cdk/aws-ec2');
-import ecs = require('@aws-cdk/aws-ecs');
+import { CertificateRef } from '@aws-cdk/aws-certificatemanager';
+import { VpcNetwork } from '@aws-cdk/aws-ec2';
+import { Cluster, ContainerImage, LoadBalancedFargateService} from '@aws-cdk/aws-ecs';
+import { HostedZoneNameRef } from '@aws-cdk/aws-route53';
 import cdk = require('@aws-cdk/cdk');
 
-class CdkStack extends cdk.Stack {
-  constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
+interface TriviaBackendStackProps extends cdk.StackProps {
+  domainName: string;
+  domainZone: string;
+}
+
+class TriviaBackendStack extends cdk.Stack {
+  constructor(parent: cdk.App, name: string, props: TriviaBackendStackProps) {
     super(parent, name, props);
 
-    // TODO VPC and cluster should be imported from common infra pipe
-    const vpc = new ec2.VpcNetwork(this, 'TriviaGameVPC', { maxAZs: 2 });
-    const cluster = new ecs.Cluster(this, 'TriviaGameCluster', { vpc });
+    const vpc = new VpcNetwork(this, 'VPC', { maxAZs: 2 });
+    const cluster = new Cluster(this, 'Cluster', { vpc });
 
-    new ecs.LoadBalancedFargateService(this, 'Service', {
+    const cert = new cdk.SSMParameterProvider(this, { parameterName: 'CertificateArn-' + props.domainName });
+
+    new LoadBalancedFargateService(this, 'Service', {
       cluster: cluster,
-      image: ecs.ContainerImage.fromAsset(this, 'Image', { directory: '../' })
+      image: ContainerImage.fromAsset(this, 'Image', { directory: '../' }),
+      desiredCount: 3,
+      domainName: props.domainName,
+      domainZone: HostedZoneNameRef.fromName(this, 'Domain', props.domainZone),
+      certificate: CertificateRef.import(this, 'Cert', { certificateArn: cert.parameterValue() })
     });
   }
 }
 
 const app = new cdk.App();
-
-new CdkStack(app, 'CdkStack');
-
+new TriviaBackendStack(app, 'TriviaBackendTest', {
+    domainName: 'api-test.reinvent-trivia.com',
+    domainZone: 'reinvent-trivia.com'
+});
+new TriviaBackendStack(app, 'TriviaBackendProd', {
+  domainName: 'api.reinvent-trivia.com',
+  domainZone: 'reinvent-trivia.com'
+});
 app.run();
