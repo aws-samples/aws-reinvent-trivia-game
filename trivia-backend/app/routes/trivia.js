5edb2ea4701857c2374e7eb24969f32781ad375c
Clare Liguori <liguori@amazon.com>
2018-11-10 17:09:51 -0800

    API for checking answers

--- trivia-backend/app/routes/trivia.js
+++ trivia-backend/app/routes/trivia.js
@@ -7,4 +7,20 @@ router.get('/all', function(req, res, next) {
   res.send(questions);
 });
 
+router.post('/check-answer/:question_id', function(req, res, next) {
+  var id = req.params.question_id;
+  var answer = req.body.answer;
+
+  var isCorrect = false;
+  questions.forEach(function(category) {
+    category.questions.forEach(function(question) {
+        if (question.id == id && question.answer == answer) {
+          isCorrect = true;
+        }
+    });
+  });
+
+  res.json({ "correct" : isCorrect });
+});
+
 module.exports = router;
