472c2e1c57c2a0cb1ff59d37f5e877411f0cbd7d
Clare Liguori <liguori@amazon.com>
2018-11-10 18:05:53 -0800

    API for retrieving question by ID

--- trivia-backend/app/routes/trivia.js
+++ trivia-backend/app/routes/trivia.js
@@ -7,20 +7,50 @@ router.get('/all', function(req, res, next) {
   res.send(questions);
 });
 
-router.post('/check-answer/:question_id', function(req, res, next) {
+// Return question content
+router.get('/question/:question_id', function(req, res, next) {
   var id = req.params.question_id;
-  var answer = req.body.answer;
 
-  var isCorrect = false;
+  var foundQuestion;
   questions.forEach(function(category) {
     category.questions.forEach(function(question) {
-        if (question.id == id && question.answer == answer) {
-          isCorrect = true;
+        if (question.id == id) {
+          foundQuestion = question;
         }
     });
   });
 
-  res.json({ "correct" : isCorrect });
+  if (foundQuestion) {
+    res.json({ "question" : foundQuestion.question });
+  } else {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
+});
+
+// Post answer to question, get result (true/false) back
+router.post('/question/:question_id', function(req, res, next) {
+  var id = req.params.question_id;
+  var answer = req.body.answer;
+
+  var foundQuestion;
+  questions.forEach(function(category) {
+    category.questions.forEach(function(question) {
+        if (question.id == id) {
+          foundQuestion = question;
+        }
+    });
+  });
+
+  if (foundQuestion) {
+    var isCorrect = (foundQuestion.answer == answer);
+    res.json({ "result" : isCorrect });
+  } else {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
 });
 
 module.exports = router;
