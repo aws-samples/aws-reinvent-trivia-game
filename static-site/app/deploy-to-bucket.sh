c97d50bc60bfdaf1ff1e7e206b4abd3b1fb0e05b
Clare Liguori <liguori@amazon.com>
2019-06-26 18:18:35 -0700

    Wait for invalidation to complete

--- static-site/app/deploy-to-bucket.sh
+++ static-site/app/deploy-to-bucket.sh
@@ -7,5 +7,7 @@ if [ ! -z "$WEBSITE_BUCKET" ]; then
 
     DISTRIBUTION=`aws cloudfront list-distributions | jq -r --arg WEBSITE_BUCKET "$WEBSITE_BUCKET" '.DistributionList.Items[] | select(.Aliases.Items[0] == $WEBSITE_BUCKET) | .Id'`
 
-    aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths "/*"
+    INVALIDATION=`aws cloudfront create-invalidation --distribution-id $DISTRIBUTION --paths "/*" --output text --query 'Invalidation.Id'`
+
+    aws cloudfront wait invalidation-completed --distribution-id $DISTRIBUTION --id $INVALIDATION
 fi
