f6bd85b66b48ae80cf57894ab04e3691a2dbde98
Clare Liguori <liguori@amazon.com>
2020-02-21 21:30:08 -0800

    Tag the rest of the things

--- trivia-backend/infra/codedeploy-blue-green/setup.sh
+++ trivia-backend/infra/codedeploy-blue-green/setup.sh
@@ -18,9 +18,9 @@ npm install
 
 aws cloudformation package --template-file template.yaml --s3-bucket $1 --output-template-file packaged-template.yaml
 
-aws cloudformation deploy --region us-east-1 --template-file packaged-template.yaml --stack-name TriviaBackendHooksTest --capabilities CAPABILITY_IAM --parameter-overrides TriviaBackendDomain=api-test.reinvent-trivia.com
+aws cloudformation deploy --region us-east-1 --template-file packaged-template.yaml --stack-name TriviaBackendHooksTest --tags project=reinvent-trivia --capabilities CAPABILITY_IAM --tags project=reinvent-trivia --parameter-overrides TriviaBackendDomain=api-test.reinvent-trivia.com
 
-aws cloudformation deploy --region us-east-1 --template-file packaged-template.yaml --stack-name TriviaBackendHooksProd --capabilities CAPABILITY_IAM --parameter-overrides TriviaBackendDomain=api.reinvent-trivia.com
+aws cloudformation deploy --region us-east-1 --template-file packaged-template.yaml --stack-name TriviaBackendHooksProd --tags project=reinvent-trivia --capabilities CAPABILITY_IAM --tags project=reinvent-trivia --parameter-overrides TriviaBackendDomain=api.reinvent-trivia.com
 
 cd ..
 
@@ -40,7 +40,7 @@ ACCOUNT_ID=`aws sts get-caller-identity --query Account --output text --region u
 
 sed -i "s|<PLACEHOLDER>|$ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/reinvent-trivia-backend:latest|g" build/task-definition-test.json build/task-definition-prod.json
 
-aws ecs create-cluster --region us-east-1 --cluster-name default
+aws ecs create-cluster --region us-east-1 --cluster-name default --tags key=project,value=reinvent-trivia
 
 aws ecs register-task-definition --region us-east-1 --cli-input-json file://build/task-definition-test.json
 
@@ -54,9 +54,9 @@ aws ecs create-service --region us-east-1 --service-name trivia-backend-prod --c
 
 aws deploy create-deployment-config --region us-east-1 --cli-input-json file://deployment-config.json
 
-aws deploy create-application --region us-east-1 --application-name AppECS-default-trivia-backend-test --compute-platform ECS
+aws deploy create-application --region us-east-1 --application-name AppECS-default-trivia-backend-test --compute-platform ECS --tags Key=project,Value=reinvent-trivia
 
-aws deploy create-application --region us-east-1 --application-name AppECS-default-trivia-backend-prod --compute-platform ECS
+aws deploy create-application --region us-east-1 --application-name AppECS-default-trivia-backend-prod --compute-platform ECS --tags Key=project,Value=reinvent-trivia
 
 aws deploy create-deployment-group --region us-east-1 --deployment-group-name DgpECS-default-trivia-backend-test --cli-input-json file://build/deployment-group-test.json
 
